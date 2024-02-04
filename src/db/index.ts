b74a28be19b7c9106c3e56ef6cc437f500261289
sharmayatendra <yatendra948@gmail.com>
2024-02-04 18:49:41 +0530

    feat:Initiate app with express

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
 
 export const connectDB = async () => {
+  console.log(":::", process.env.MONGODB_URI);
+
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`✅✅✅ MongoDB connected`);
+    console.log(
+      `✅✅ MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MONGODB connection error", error);
     process.exit(1);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-const dotenv = require("dotenv").config();
+import * as dotenv from "dotenv";
 
+import { app } from "./app";
 import { connectDB } from "./db/index";
 
-// dotenv.config({
-//   path: "./env",
-// });
+dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() =>
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => console.log("❌❌❌MONGODB connection failed: ", err));
 
 // console.log("From index.ts file!!!");
