f405ecf9226c5a243956c938be001c15c27d68de
sharmayatendra <yatendra948@gmail.com>
2024-03-17 09:18:51 +0530

    feat: Add upload file functionality
    - Add a utility function `uploadFileOnCloudinary` for file upload

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFileOnCloudinary = async (filePath: string) => {
+  try {
+    if (!filePath) return null;
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(filePath, {
+      format: "auto",
+    });
+    console.log("file uploaded successfully", response);
+    return response;
+  } catch (error) {
+    // remove the file from local if somehow it fails to upload on cloudinary
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
